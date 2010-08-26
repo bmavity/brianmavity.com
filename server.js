1e7a92ea3f9885548d3ccb98fa40b5d92c139f6b
Brian Mavity <brian@brianmavity.com>
2010-08-26 18:09:39 -0500

    changed output to html

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var http = require('http');
  
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Evenin', Bitches!");
+  res.writeHead(200, { "Content-Type": "text/html" })
+  var html = '<! DOCTYPE html>';
+  html += '<html>\n';
+  html += '  <head>\n';
+  html += '    <title>Chapelle Inspired Hello World</title>\n';
+  html += '  </head>\n';
+  html += '  <body>\n';
+  html += '    <h1>Evenin&rsquo;, Bitches!</h1>\n';
+  html += '  </body>\n';
+  html += '</html>';
+  res.end(html);
 });
  
-server.listen(80);
+server.listen(8000);

a9fba6f949e3dbae30bf4343baeb95b077c97ccc
Brian Mavity <brian@brianmavity.com>
2010-09-07 13:37:10 -0500

    Updated compiler extensibility syntax

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ var sys = require('sys'),
     app = require('express').createServer(),
     pub = __dirname + '/public';
 
+connect.compiler.compilers['scss'] = require('scss/compiler');
+
 app.set('view engine', 'jade');
 
 app.use(connect.compiler({
   src: pub,
   enable: ['scss', 'sass'],
-  compilers: [{ name: 'scss', compiler: require('scss/compiler') }]
 }));
 app.use(connect.staticProvider(pub));
 
 app.get('/', function(req, res) {
   res.render('home', {});
+});
+
+app.get('/contact', function(req, res) {
+  res.render('contact', {});
 });
 
 
